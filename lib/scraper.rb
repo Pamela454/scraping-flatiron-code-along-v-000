8736e607936812bfc48383f95dedfd9b15e8e902
Pamela Frieze <pfrieze@gmail.com>
2018-03-17 15:24:22 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,8 +12,8 @@ class Scraper
         course = Course.new
         course.title = post.css("h2").text
         course.schedule = post.css(".date").text
-        course.description = post.css("p").text
         binding.pry
+        course.description = post.css("p").text
     end
 
     def get_courses
